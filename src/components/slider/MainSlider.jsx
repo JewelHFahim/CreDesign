8f16aa5c9306f00437678ad9be129269163714b3
JewelHFahim <jewelhfahim@gmail.com>
2024-08-29 01:02:42 +0600

    updated slide

--- src/components/slider/MainSlider.jsx
+++ src/components/slider/MainSlider.jsx
@@ -4,7 +4,30 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 import "./MainSlider.css";
 import "swiper/css";
-import img1 from "../../assets/bg.jpg";
+import img1 from "../../assets/casino4.jpg";
+import img2 from "../../assets/casino2.jpg";
+import img3 from "../../assets/casino3.jpg";
+
+const images = [
+  {
+    id: 1,
+    img: img1,
+    title: "ARE YOU READY?",
+    des: "YOU COULD WALK AWAY WITH SOME EPIC CASH PRIZES!",
+  },
+  {
+    id: 2,
+    img: img2,
+    title: "JOIN THE REVELRY",
+    des: "FOR YOUR SHOT AT ROUNDS OF FREE SPINS!",
+  },
+  {
+    id: 3,
+    img: img3,
+    title: "Bonus on your 1st deposit",
+    des: "100% UP TO $1000",
+  },
+];
 
 const MainSlider = () => {
   return (
@@ -24,10 +47,13 @@ const MainSlider = () => {
         modules={[Autoplay, Pagination, Navigation]}
         className="z-[-1]"
       >
-        {[1, 2, 3, 4].map((item, i) => (
+        {images.map((item, i) => (
           <SwiperSlide key={i}>
-            <div className="w-full lg:h-[450px]">
-              <img src={img1} alt="" className="object-cover" />
+            <div className="w-full lg:h-[450px] flex justify-start items-center bg-opacity-30 px-20" style={{backgroundImage: `url(${item.img})`, backgroundPosition: "center", backgroundSize: "cover"}}>
+              <div className="flex flex-col gap-4">
+                <p className="text-3xl font-semibold text-white">{item.title}</p>
+                <h1 className="text-5xl font-semibold text-orange-600 max-w-[750px]">{item.des}</h1>
+              </div>
             </div>
           </SwiperSlide>
         ))}
